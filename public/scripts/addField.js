a2c3665691102ebbd5ea2555c73c4b286457c02a
Halex Viotto Gomes <halexx002@gmail.com>
2020-08-28 17:03:48 -0400

    A delete icon was added to the project. The functionality to remove a row of time inputs was added A delete button was added to the section of the form where the proffy's times are added

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -30,15 +30,48 @@ function addTimeFields() {
 
 }
 
+// It deletes a set of time fields
+// "this" refers to the delete button element that was clicked
+// The function deletes the row of inputs that the button belongs
+function deleteTimeFields() {
+  const parentElement = this.parentElement;
+
+  parentElement.remove();
+}
 
 function cloneField(timeFields) {
   const newFieldContainer = timeFields.cloneNode(true);
 
   const fields = newFieldContainer.querySelectorAll('input');
 
+  // It verifies if the container element already have a delete button element
+  let deleteButtonEl = newFieldContainer.querySelector('.delete-time');
+
   fields.forEach(function(field) {
     field.value="";
   });
+
+  // It creates a new delete button element and appends to the container if the container copied doesn't have this button
+  // The first and default container doesn't have the delete button
+  if ( deleteButtonEl === null) {
+    deleteButtonEl = document.createElement('button');
+
+    deleteButtonEl.setAttribute('type', 'button');
+    deleteButtonEl.setAttribute('class', 'delete-time');
+
+    deleteButtonEl.addEventListener('click', deleteTimeFields);
+
+    deleteButtonEl.innerHTML = '<img src="/images/icons/delete.svg" alt="Delete">';
+
+    newFieldContainer.appendChild(deleteButtonEl);
+
+  }
+  else {
+    // the method cloneNode() doesn't copy the event handlers
+    // This line adds an event listener to the delete button element
+    // This event deletes the row of time inputs
+    deleteButtonEl.addEventListener('click', deleteTimeFields);
+  }
 
   document.querySelector('#schedule-items').appendChild(newFieldContainer);
 }
